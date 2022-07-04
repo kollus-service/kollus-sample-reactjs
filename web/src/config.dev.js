fcc0eda225f6a113a62bc17e09ce7b1e1dc9256e
6lueparr0t <6lueparr0t@gmail.com>
2022-07-04 10:12:33 +0900

    update config

---
+++ web/src/config.dev.js
@@ -0,0 +1,6 @@
+export const DEFAULT_TITLE = 'Kollus ReactJS Sample';
+export const DEFAULT_MCKEY = '8PlD3cHV';
+export const BASE_URL = 'http://localhost:9081'; // http://ldh.videoclouds.net
+export const SOCKET_URL = 'http://localhost:9082'; // http://ldh.videoclouds.net
+export const VG_URL = 'https://v.kr.kollus.com';
+export const KOLLUS_DOWNLOAD = 'kollus://download';
---
+++ web/src/config.prod.js
@@ -0,0 +1,6 @@
+export const DEFAULT_TITLE = 'Kollus ReactJS Sample';
+export const DEFAULT_MCKEY = '8PlD3cHV';
+export const BASE_URL = 'http://ldh.videoclouds.net';
+export const SOCKET_URL = 'http://ldh.videoclouds.net';
+export const VG_URL = 'https://v.kr.kollus.com';
+export const KOLLUS_DOWNLOAD = 'kollus://download';
